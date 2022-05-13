302b6bb199d3d506188d1bd4debd170a9e22474a
petialetia <petialetia@yandex.ru>
2022-05-13 22:07:01 +0300

    TODO-s are added

--- include/DrawningInteraction.hpp
+++ include/DrawningInteraction.hpp
@@ -94,6 +94,8 @@ class Draw
             }
             else
             {
+                //TODO: user shouldn't see this message, logging is needed
+                //TODO: for user to see that kind of problem FPS counter is needed
                 std::cout << "FPS downfall is detected on " << current_ticks << " ticks" << std::endl;
                 last_draw_ticks_.value() = current_ticks;
             }
