695a38f721c9c9147ee93df9d6613aea21c9cb71
petialetia <petialetia@yandex.ru>
2022-05-15 16:45:46 +0300

    StepByStepSimulation is better

--- src/StepByStepSimulation.cpp
+++ src/StepByStepSimulation.cpp
@@ -17,3 +17,45 @@ void MoveOnOffset(object_coordinates& object_coordinates, offset_type offset)
 {
     object_coordinates += offset;
 }
+
+const time_type& StepByStepSimulation::GetGlobalTime()
+{
+    return global_time_;
+}
+
+ObjectStorage& StepByStepSimulation::GetObjects()
+{
+    return object_storage_;
+}
+
+InteractionStorage& StepByStepSimulation::GetInteractions()
+{
+    return interaction_storage_;
+}
+
+
+void StepByStepSimulation::AddSubscriptionByDefault(std::function<void(id_type)> subscription)
+{
+    subscriptions_by_default_.push_back(subscription);
+}
+
+void StepByStepSimulation::SubscribeToDefaultInteraction(id_type id)
+{
+    for (auto& AddSubscription : subscriptions_by_default_)
+    {
+        AddSubscription(id);
+    }
+}
+
+void StepByStepSimulation::CheckInteractions()
+{
+    interaction_storage_.CheckInteractions();
+}
+
+void StepByStepSimulation::Step()
+{
+    MoveObjects(object_storage_, time_step_);
+    global_time_+= time_step_;
+
+    CheckInteractions();
+}
